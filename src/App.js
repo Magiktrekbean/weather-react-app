6af968a401cfea57625df36f841df92ed1f6eff6
Magiktrekbean <dlockettkyla@gmail.com>
2025-05-12 02:45:12 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App"><div className="container">
-      <h1>Weather App</h1>
+    
       <Weather />
      <footer>This code is
-      <a href="www.https://github.com/Magiktrekbean/weather-react-app.com" className='text-decoration-none'>
+      <a href="https://github.com/Magiktrekbean/weather-react-app"target='blank' className='text-decoration-none'>
      {' '}open sourced on github </a>
-      written by <a href="kylalockett.com" className='text-decoration-none'>
+      written by <a href="https://kylalockett.com/" className='text-decoration-none'target='blank'>
       Kyla Lockett</a>
     </footer> 
   </div>
--- src/Weather.js
+++ src/Weather.js
@@ -2,31 +2,31 @@ import React from "react";
 import "./Weather.css"
 export default function Weather (){
     return( <div className="Weather">
-      <form >
+      <form  >
         <div className="row">
         <div className="col-9">
-        <input type="search" placeholder="search for a city..." className="form-control p-3"/>
+        <input type="search"  placeholder="search for a city..." className="form-control p-3" autoFocus="on"/>
         </div>
         <div className="col-3 d-flex justify-content-end">
-        <input type="submit" value={"Search"} className="btn btn-primary ps-5 pe-5 "/>
+        <input type="submit" value={"Search"} className="btn btn-primary w-100 "/>
       </div></div></form>
       
-      <p className="m-2">Results for Lexington, KY <a href="/">choose area</a></p>
+      <p1 className ="m-1"> <span>Results for</span> Lexington, KY · <a href="/" target="blank">Choose area</a></p1>
    <div className="row">
     <div className="col-3">
-       <h1> <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloud"/> 21°C</h1>
+     <h1 className="d-flex align-items-center"> <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="cloud" /> 21<span>°C</span></h1>
     </div>
    
-        <div className="col-3">
+        <div className="col-4 ps-4">
             <ul className="list-inline">
             <li>Precipitation: 0%</li>
             <li>Humidity: 47%</li>
             <li>Wind: 13 km/h</li>
             </ul>
         </div>
-        <div className="col-6 ">
+        <div className="col-5 ">
             <h2 className="d-flex justify-content-end">Weather</h2>
-                  <p className="d-flex justify-content-end text-end">Sunday 11:00PM <br /> Cloudy</p>
+                  <p2 className="d-flex justify-content-end text-end">Sunday 11:00PM <br /> Cloudy</p2>
         </div> 
 
     </div> <hr/>
