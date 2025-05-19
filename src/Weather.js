028c133555f8aecb4ca3aeac23ab2813fb3baff4
Magiktrekbean <dlockettkyla@gmail.com>
2025-05-19 17:33:12 -0400

    fixing responsiveness

--- src/Weather.js
+++ src/Weather.js
@@ -31,13 +31,13 @@ function handleSubmit(event) {
       };
       return date.toLocaleString(undefined, options);
   }
-  
+
   function formatDay(timestamp) {
       let date = new Date(timestamp * 1000);
       const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
       return days[date.getDay()];
   }
-  
+
   const DateDisplay = ({ timestamp }) => {
       const date = new Date(timestamp * 1000);
       return (
@@ -73,24 +73,25 @@ if (data.ready){
     return( <div className="Weather">
       <div className="container">
        <div className="p-3 d-block">
-      <form  onSubmit={handleSubmit}  >
+      <form  onSubmit={handleSubmit} >
         <div className="row">
-        <div className="col-sm-9 input">
-        <input type="search" onChange={updateCity} placeholder="search for a city..." className="form-control p-3" id="city-search" autoFocus="on"/>
+        <div className="col-sm-9 col-12 input">
+        <input type="search" onChange={updateCity} placeholder="search for a city..." className="form-control p-3" autoFocus="on"/>
         </div>
-        <div className="col-sm-3 input d-flex justify-content-end">
-        <input type="submit" value={"Search"} className="btn btn-primary w-100 " id="press-search"/>
+        <div className="col-sm-3 col-12 input d-flex justify-content-end">
+        <input type="submit" value={"Search"} className="btn btn-primary w-100 "/>
       </div></div></form>
-      
+
       <p > <span>Results for</span> {data.city}, {data.country} · <a href="/" onClick={showAlert}>Choose area</a></p>
-  
-  
-   <div className="row adjusted ">
+
+
+   <div className="row">
     <span className=" d-block d-sm-none "><div className="text-capitalize mb-3 title fw-bold ">Weather</div><span className="time"><DateDisplay  timestamp={data.time}/></span> </span>
-     
-       <div className="col-sm d-flex align-items-start ">
-        <div className="col-3 ms-0"><h1 className="d-flex align-items-center "> <img src={data.iconUrl} alt={data.icon}/> <WeatherUnit celsius= {data.temperature} /></h1></div> 
-       <div className="col-3 me-0">
+
+      
+       <div className=" col d-flex align-items-start ">
+        <div className="col ms-0"><h1 className="d-flex align-items-center "> <img src={data.iconUrl} alt={data.icon}/> <WeatherUnit celsius= {data.temperature} /></h1></div> 
+       <div className="col m-0">
 
             <ul className="list-inline  ps-0 text-start">
             <li className="d-block d-sm-none text-capitalize description fw-bold"> {data.description}</li>
@@ -98,9 +99,9 @@ if (data.ready){
             <li className="">Wind: {data.wind} km/h</li>
             </ul>   
         </div>
-      
-        
-        <div className="col-sm small">
+
+
+        <div className="col small">
             <h2 className="">Weather</h2>
                   <span className="time  "> <DateDisplay timestamp={data.time}/><div className="text-capitalize">{data.description}</div></span>
         </div>   
@@ -108,10 +109,10 @@ if (data.ready){
 
 <WeatherForecast coordinates ={data.coordinates}/>
  <hr/>
-   
+
      </div></div>);} else {
       const apiKey =`594b61tf99f8e42c306162ocb32f8ac6`;
-  
+
       let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(url).then(showData);
       return "Loading";
